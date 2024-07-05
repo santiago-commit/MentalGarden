de29405adc95fec5762dc1f9d9cc165edee3c9ff
santi <santimanzanilla24@gmail.com>
2024-07-05 17:03:18 -0400

    First Commit

---
+++ index.js
@@ -0,0 +1,34 @@
+function set_date() {
+	const months = ["JANUARY", "FEBRUARY", "MARCH", "APRIL", "MAY", "JUNE", "JULY", "AUGUST", "SEPTEMBER", "OCTOBER", "NOVEMBER", "DECEMBER"]
+	var month = document.getElementById("month")
+
+	const date = new Date()
+	month.innerHTML = months[date.getMonth()] + " <em>" + date.getFullYear() + "</em>"
+}
+
+function show_time(){
+	var date = new Date()
+	var h = date.getHours()
+	var m = date.getMinutes()
+
+	h = (h < 10) ? "0" + h : h
+	m = (m < 10) ? "0" + m : m
+
+	document.getElementById("hour").innerText = h + ":" + m
+	setTimeout(show_time, 1000)
+}
+
+set_date()
+show_time()
+
+function toggle_navbar() {
+	const toggle_button = document.querySelector(".toggle-button")
+	toggle_button.addEventListener('click', function () {
+		document.getElementsByTagName("nav")[0].classList.toggle('active')
+	})
+}
+
+toggle_navbar()
+
+
+
